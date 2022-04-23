1d945b4c11113e9241dfdae768693243845935e2
akaRicane <philippe.guelen@gmail.com>
2022-04-23 18:33:31 +0200

    recursive parsing

--- server/index.js
+++ server/index.js
@@ -1,40 +1,26 @@
 const express = require("express");
 const cors = require('cors');
-const https = require('https');
-const path = require('path');
 const fs = require('fs');
-const util = require('util')
+const https = require('https');
+
+var recursive = require('recursive-readdir');
 
 const APP = express();
 APP.use(cors());
 const PORT = 3001;
-const ASSETS_DIR = '/Users/ricane/Documents/Projets/coding/mapping/public/assets/fonts';
-
-// APP.use(express.static('public'));
+const ASSETS_DIR = '/Users/ricane/Documents/Projets/coding/mapping/public/assets';
 
 APP.get("/", (req, res) => { console.log(req); res.send("This is from express"); });
 
 APP.get("/analyze", async (req, res) => {
-    console.log("\n\nNew request for analysis\n");
-    await readDirectory(ASSETS_DIR)
-        .then(filenames => {
-            var assetContent = [];
-            filenames.forEach(file => {
-                assetContent.push(path.join(ASSETS_DIR, file));
-            });
-
-            console.log(assetContent);
-            res.send(assetContent);
-        })
-        // If promise is rejected
-        .catch(err => {
-            console.log("Error code -> ${err.code}");
-            res.send('Fail');
-        });
-});
-
-APP.post('/', (req, res) => {
-    res.send("Got a POST request");
+    console.log("\n\nNew request to Analyze assets");
+    var dirReader = [];
+    await recursive(ASSETS_DIR)
+    .then((files) => {
+        for (const [key, file] of Object.entries(files)) {dirReader.push(file);}
+        console.log("Assets analysis is done ! (found: " + dirReader.length + ")\n");
+        res.send(dirReader);
+    })
 });
 
 const server = https.createServer({
@@ -43,46 +29,3 @@ const server = https.createServer({
   }, APP);
 
 server.listen(PORT, () => { console.log("Express.js server listening on PORT: " + PORT) });
-
-const readDir = util.promisify(fs.readdir)
-
-const readDirectory = async (path) => {
-    const filenames = await readDir(path)
-    console.log(typeof filenames)
-    return filenames;
-}
-
-
-const listDirContent = async (folder) => {
-    var tempReader = [];
-    console.log("Listing content of: " + folder);
-    await fs.readdir(folder, (err, files) => {
-        if (err) {
-            console.log(err);
-        }
-        else {
-            // console.log(files);
-            files.forEach(file => {
-                var dest = path.join(folder, file);
-                if (fs.lstatSync(dest).isDirectory()) {
-                    // console.log('Folder found: ' + dest);
-                    console.log('Folder found');
-                    // listDirContent(dest);
-                    // assetContent = listDirContent(ASSETS_DIR, assetContent);
-                }
-                else if (!fs.lstatSync(dest).isFile()) {
-                    console.log('Unknown : ' + dest);
-                }
-                else {
-                    tempReader.push(String(dest));
-                }
-            })
-        }
-        console.log('Current number of assets: ' + tempReader.length);
-        console.log("Temp from inside");
-        console.log(tempReader);
-        return tempReader;
-    });
-}
-
-const listDirContentAsync = util.promisify(listDirContent)
